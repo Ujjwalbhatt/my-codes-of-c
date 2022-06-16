1623ff9737a2023785a6d449436f5ed11d7d40ee
Ujjwal <Ujjwalbhatt04@gmail.com>
2022-06-16 21:18:49 +0530

    j

---
+++ exam practice/ceilandfloor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    int n,min,x;
+    printf("Enter the range of the arrays: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the value you want to find: ");
+    scanf("%d",&x);
+    for (int i = 0; i < n; i++)
+    {
+        
+    scanf("%d",)
+    }
+    
+    return 0;
+}
