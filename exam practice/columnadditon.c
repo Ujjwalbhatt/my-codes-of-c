fade546841fd7e450e2c31254135bad1feba0ec2
Ujjwal <Ujjwalbhatt04@gmail.com>
2022-06-18 08:44:01 +0530

    mywork

---
+++ exam practice/columnadditon.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main () {
+    int a,b;
+int sum=0;
+    printf("Enter the rows and columns arrays: ");
+    scanf("%d %d",&a,&b);
+    int arr[a][b];
+    printf("Enter the element of the arrays: \n");
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+        
+    }
+    for (int i = 0; i < a; i++)
+    {
+        sum=0;
+        for (int j = 0; j < b; j++)
+        {
+            sum=sum+arr[i][j];
+        }
+            printf("%d ",sum);
+        
+    }
+    
+    
+    return 0;
+}
