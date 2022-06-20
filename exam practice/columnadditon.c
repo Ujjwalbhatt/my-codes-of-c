738842a1bb1490da9ceff88a6b70734dae037d70
Ujjwal <Ujjwalbhatt04@gmail.com>
2022-06-20 10:05:23 +0530

    today

--- exam practice/columnadditon.c
+++ exam practice/columnadditon.c
@@ -6,6 +6,7 @@ int sum=0;
     printf("Enter the rows and columns arrays: ");
     scanf("%d %d",&a,&b);
     int arr[a][b];
+    int summ[a];
     printf("Enter the element of the arrays: \n");
     for (int i = 0; i < a; i++)
     {
@@ -22,10 +23,19 @@ int sum=0;
         {
             sum=sum+arr[i][j];
         }
-            printf("%d ",sum);
+           summ[i]=sum;
         
     }
     
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            printf("%d   ",arr[i][j]);
+        }
+        printf(" |  %d \n",summ[i]);
+        
+    }
     
     return 0;
 }
--- exam practice/rowsaddition.c
+++ exam practice/rowsaddition.c
@@ -6,6 +6,7 @@ int sum=0;
     printf("Enter the rows and columns arrays: ");
     scanf("%d %d",&a,&b);
     int arr[a][b];
+    int summ[b];
     printf("Enter the element of the arrays: \n");
     for (int i = 0; i < a; i++)
     {
@@ -17,14 +18,34 @@ int sum=0;
     }
     for (int j = 0; j < b; j++)
     {
+  
             sum=0;
         for (int i = 0; i < b; i++)
         {
+            
         sum=sum+arr[i][j];
         }
-            printf("%d ",sum);
+            summ[j]=sum;
+            
         
     }
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            
+        printf("%d ",arr[i][j]);
+        }
+        printf("\n"); 
+        
+    }
+    printf("----------\n");
+    for (int i = 0; i < b; i++)
+    {
+        printf("%d ",summ[i]);
+    }
+    
+    
     
     
     return 0;
